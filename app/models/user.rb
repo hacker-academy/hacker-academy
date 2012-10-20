ff36ca5754d28abaa82bfc729f9ba220d452a963
Adam Hueniken <adam.hueniken@gmail.com>
2012-10-20 15:48:01 -0400

    C3 Teams added

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User
   include Mongoid::Document
   validates_uniqueness_of :name, :email, :case_sensitive => false
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
-  attr_accessible :preferred_language, :year, :program, :rfid, :shirt_size
+  attr_accessible :team, :preferred_language, :year, :program, :rfid, :shirt_size
 
   devise :database_authenticatable, :registerable, :recoverable,
     :rememberable, :trackable, :validatable, :lockable
