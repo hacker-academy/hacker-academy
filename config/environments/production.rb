dd11395d9abb795d794036d7c08270a07e6f2426
Adam Hueniken <adam.hueniken@gmail.com>
2012-10-27 14:56:30 -0400

    action mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,6 +8,8 @@ HackerAcademy::Application.configure do
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  config.action_mailer.default_url_options = { :host => 'www.hackeracademy.org' }
+
 
   # Specifies the header that your server uses for sending files
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
@@ -46,7 +48,5 @@ HackerAcademy::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-
-  config.action_mailer.default_url_options = { :host => 'www.hackeracademy.org' }
 
 end
